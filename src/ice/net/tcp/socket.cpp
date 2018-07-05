8b6c74ff096655a6faca21915524b62046ca27c1
Alexej Harm <alexej.h@xiphos.de>
2018-07-05 17:56:53 +0200

    added tcp socket

---
+++ src/ice/net/tcp/socket.cpp
@@ -0,0 +1,417 @@
+#include "socket.hpp"
+#include <ice/error.hpp>
+#include <cassert>
+
+#if ICE_OS_WIN32
+#  include <windows.h>
+#  include <winsock2.h>
+#  include <mswsock.h>
+#  include <ws2tcpip.h>
+#  include <new>
+#else
+#  include <netinet/in.h>
+#  include <sys/socket.h>
+#  include <sys/types.h>
+#  include <unistd.h>
+#endif
+
+namespace ice::net::tcp {
+namespace detail {
+
+#if ICE_OS_WIN32
+
+struct connect_ex {
+  connect_ex()
+  {
+    net::socket::handle_type socket(::WSASocketW(AF_INET, SOCK_STREAM, 0, nullptr, 0, WSA_FLAG_OVERLAPPED));
+    if (!socket) {
+      ec = make_error_code(::WSAGetLastError());
+      return;
+    }
+    auto fp = SIO_GET_EXTENSION_FUNCTION_POINTER;
+    auto guid = GUID(WSAID_CONNECTEX);
+    auto size = DWORD(sizeof(function));
+    if (::WSAIoctl(socket, fp, &guid, sizeof(guid), &function, size, &size, nullptr, nullptr) == SOCKET_ERROR) {
+      ec = make_error_code(::WSAGetLastError());
+      return;
+    }
+  }
+
+  template <typename... Args>
+  auto operator()(Args&&... args) const noexcept
+  {
+    assert(function);
+    return function(std::forward<Args>(args)...);
+  }
+
+  std::error_code ec;
+  LPFN_CONNECTEX function = nullptr;
+};
+
+#endif
+
+}  // namespace detail
+
+socket::socket(ice::service& service, int family) : net::socket(service, family, SOCK_STREAM, IPPROTO_TCP) {}
+
+socket::socket(ice::service& service, int family, int protocol) : net::socket(service, family, SOCK_STREAM, protocol) {}
+
+std::error_code socket::create(int family) noexcept
+{
+  return net::socket::create(family, SOCK_STREAM, IPPROTO_TCP);
+}
+
+std::error_code socket::create(int family, int protocol) noexcept
+{
+  return net::socket::create(family, SOCK_STREAM, protocol);
+}
+
+std::error_code socket::listen(std::size_t backlog)
+{
+  const auto size = backlog > 0 ? static_cast<int>(backlog) : SOMAXCONN;
+#if ICE_OS_WIN32
+  if (::listen(handle_, size) == SOCKET_ERROR) {
+    return make_error_code(::WSAGetLastError());
+  }
+#else
+  if (::listen(handle_, size) < 0) {
+    return make_error_code(errno);
+  }
+#endif
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+  ::linger data = { 1, 0 };
+  ::setsockopt(handle_, SOL_SOCKET, SO_LINGER, &data, sizeof(data));
+#endif
+  return {};
+}
+
+#if 0
+bool accept::await_ready() noexcept
+{
+#  if ICE_OS_LINUX || ICE_OS_FREEBSD
+  client_.endpoint().size() = client_.endpoint().capacity();
+  auto& sockaddr = client_.endpoint().sockaddr();
+  auto& size = client_.endpoint().size();
+  client_.handle().reset(::accept4(socket_, &sockaddr, &size, SOCK_NONBLOCK));
+  if (client_) {
+    return true;
+  }
+  if (errno != EAGAIN && errno != EINTR) {
+    ec_ = errno;
+    return true;
+  }
+#  endif
+  return false;
+}
+
+bool accept::suspend() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<SOCKET>();
+  const auto client = client_.handle().as<SOCKET>();
+  while (true) {
+    if (::AcceptEx(socket, client, &buffer_, 0, buffer_size, buffer_size, &bytes_, get())) {
+      break;
+    }
+    const auto rc = ::WSAGetLastError();
+    if (rc == ERROR_IO_PENDING) {
+      return true;
+    }
+    if (rc != WSAECONNRESET) {
+      ec_ = rc;
+      return false;
+    }
+  }
+  return false;
+#  else
+  return queue_recv(service_, socket_);
+#  endif
+}
+
+bool accept::resume() noexcept
+{
+#  if ICE_OS_WIN32
+  DWORD bytes = 0;
+  if (!::GetOverlappedResult(socket_.as<HANDLE>(), get(), &bytes, FALSE)) {
+    if (const auto rc = ::GetLastError(); rc != WSAECONNRESET) {
+      ec_ = rc;
+      return true;
+    }
+    return false;
+  }
+  const auto addr = reinterpret_cast<::sockaddr_storage*>(&buffer_[buffer_size]);
+  switch (addr->ss_family) {
+  case AF_INET:
+    client_.endpoint().sockaddr_in() = *reinterpret_cast<::sockaddr_in*>(addr);
+    client_.endpoint().size() = sizeof(::sockaddr_in);
+    break;
+  case AF_INET6:
+    client_.endpoint().sockaddr_in6() = *reinterpret_cast<::sockaddr_in6*>(addr);
+    client_.endpoint().size() = sizeof(::sockaddr_in6);
+    break;
+  }
+  return true;
+#  else
+  return await_ready();
+#  endif
+}
+
+connect::connect(tcp::socket& socket, const net::endpoint& endpoint) noexcept :
+  service_(socket.service().handle()), socket_(socket.handle()), endpoint_(endpoint)
+{
+#  if ICE_OS_WIN32
+  sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = 0;
+  if (::bind(socket_, reinterpret_cast<const SOCKADDR*>(&addr), sizeof(addr)) == SOCKET_ERROR) {
+    ec_ = ::WSAGetLastError();
+  }
+#  endif
+  socket.endpoint() = endpoint;
+}
+
+bool connect::await_ready() noexcept
+{
+#  if ICE_OS_LINUX || ICE_OS_FREEBSD
+  while (true) {
+    if (::connect(socket_, &endpoint_.sockaddr(), endpoint_.size()) == 0) {
+      return true;
+    }
+    if (errno == EINPROGRESS) {
+      break;
+    }
+    if (errno == EINTR) {
+#    if ICE_OS_LINUX
+      continue;
+#    else
+      break;
+#    endif
+    }
+    ec_ = errno;
+    break;
+  }
+#  endif
+  return false;
+}
+
+bool connect::suspend() noexcept
+{
+#  if ICE_OS_WIN32
+  static const detail::connect_ex connect;
+  if (connect.ec) {
+    ec_ = connect.ec;
+    return false;
+  }
+  const auto socket = socket_.as<SOCKET>();
+  if (connect(socket, &endpoint_.sockaddr(), endpoint_.size(), nullptr, 0, nullptr, get())) {
+    return false;
+  }
+  if (const auto rc = ::WSAGetLastError(); rc != ERROR_IO_PENDING) {
+    ec_ = rc;
+    return false;
+  }
+  return true;
+#  else
+  return queue_send(service_, socket_);
+#  endif
+}
+
+bool connect::resume() noexcept
+{
+#  if ICE_OS_WIN32
+  DWORD bytes = 0;
+  const auto socket = socket_.as<HANDLE>();
+  if (!::GetOverlappedResult(socket, get(), &bytes, FALSE)) {
+    ec_ = ::GetLastError();
+  }
+#  else
+  auto code = 0;
+  auto size = static_cast<socklen_t>(sizeof(code));
+  if (::getsockopt(socket_, SOL_SOCKET, SO_ERROR, &code, &size) < 0) {
+    ec_ = errno;
+  } else if (code) {
+    ec_ = code;
+  }
+#  endif
+  return true;
+}
+
+bool recv::await_ready() noexcept
+{
+#  if ICE_OS_LINUX || ICE_OS_FREEBSD
+  if (const auto rc = ::read(socket_, buffer_.data, buffer_.size); rc >= 0) {
+    buffer_.size = static_cast<std::size_t>(rc);
+    return true;
+  }
+  if (errno == ECONNRESET) {
+    buffer_.size = 0;
+    return true;
+  }
+  if (errno != EAGAIN && errno != EINTR) {
+    ec_ = errno;
+    return true;
+  }
+#  endif
+  return false;
+}
+
+bool recv::suspend() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<SOCKET>();
+  const auto buffer = std::launder(reinterpret_cast<LPWSABUF>(&buffer_));
+  if (::WSARecv(socket, buffer, 1, &bytes_, &flags_, get(), nullptr) != SOCKET_ERROR) {
+    buffer_.size = bytes_;
+    return false;
+  }
+  if (const auto rc = ::WSAGetLastError(); rc != ERROR_IO_PENDING) {
+    ec_ = rc;
+    return false;
+  }
+  return true;
+#  else
+  return queue_recv(service_, socket_);
+#  endif
+}
+
+bool recv::resume() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<HANDLE>();
+  if (!::GetOverlappedResult(socket, get(), &bytes_, FALSE)) {
+    ec_ = ::GetLastError();
+  }
+  buffer_.size = bytes_;
+  return true;
+#  else
+  return await_ready();
+#  endif
+}
+
+bool send::await_ready() noexcept
+{
+#  if ICE_OS_LINUX || ICE_OS_FREEBSD
+  if (const auto rc = ::write(socket_, buffer_.data, buffer_.size); rc > 0) {
+    assert(buffer_.size >= static_cast<std::size_t>(rc));
+    buffer_.data += static_cast<std::size_t>(rc);
+    buffer_.size -= static_cast<std::size_t>(rc);
+    size_ += static_cast<std::size_t>(rc);
+    return buffer_.size == 0;
+  } else if (rc == 0) {
+    return true;
+  }
+  if (errno != EAGAIN && errno != EINTR) {
+    ec_ = errno;
+    return true;
+  }
+#  endif
+  return false;
+}
+
+bool send::suspend() noexcept
+{
+#  if ICE_OS_WIN32
+  while (buffer_.size > 0) {
+    const auto socket = socket_.as<SOCKET>();
+    const auto buffer = std::launder(reinterpret_cast<LPWSABUF>(&buffer_));
+    if (::WSASend(socket, buffer, 1, &bytes_, 0, get(), nullptr) == SOCKET_ERROR) {
+      if (const auto rc = ::WSAGetLastError(); rc != ERROR_IO_PENDING) {
+        ec_ = rc;
+        break;
+      }
+      return true;
+    }
+    buffer_.data += bytes_;
+    buffer_.size -= bytes_;
+    size_ += bytes_;
+    if (bytes_ == 0 || buffer_.size == 0) {
+      break;
+    }
+  }
+  return false;
+#  else
+  return queue_send(service_, socket_);
+#  endif
+}
+
+bool send::resume() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<HANDLE>();
+  if (!::GetOverlappedResult(socket, get(), &bytes_, FALSE)) {
+    ec_ = ::GetLastError();
+  } else {
+    buffer_.data += bytes_;
+    buffer_.data -= bytes_;
+    size_ += bytes_;
+    if (bytes_ > 0 && buffer_.size > 0) {
+      return false;
+    }
+  }
+  return true;
+#  else
+  return await_ready();
+#  endif
+}
+
+bool send_some::await_ready() noexcept
+{
+#  if ICE_OS_LINUX || ICE_OS_FREEBSD
+  if (const auto rc = ::write(socket_, buffer_.data, buffer_.size); rc > 0) {
+    assert(buffer_.size >= static_cast<std::size_t>(rc));
+    buffer_.data += static_cast<std::size_t>(rc);
+    buffer_.size -= static_cast<std::size_t>(rc);
+    size_ += static_cast<std::size_t>(rc);
+    return true;
+  } else if (rc == 0) {
+    return true;
+  }
+  if (errno != EAGAIN && errno != EINTR) {
+    ec_ = errno;
+    return true;
+  }
+#  endif
+  return false;
+}
+
+bool send_some::suspend() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<SOCKET>();
+  const auto buffer = std::launder(reinterpret_cast<LPWSABUF>(&buffer_));
+  if (::WSASend(socket, buffer, 1, &bytes_, 0, get(), nullptr) == SOCKET_ERROR) {
+    if (const auto rc = ::WSAGetLastError(); rc != ERROR_IO_PENDING) {
+      ec_ = rc;
+      return false;
+    }
+    return true;
+  }
+  buffer_.data += bytes_;
+  buffer_.size -= bytes_;
+  size_ += bytes_;
+  return false;
+#  else
+  return queue_send(service_, socket_);
+#  endif
+}
+
+bool send_some::resume() noexcept
+{
+#  if ICE_OS_WIN32
+  const auto socket = socket_.as<HANDLE>();
+  if (!::GetOverlappedResult(socket, get(), &bytes_, FALSE)) {
+    ec_ = ::GetLastError();
+  } else {
+    buffer_.data += bytes_;
+    buffer_.data -= bytes_;
+    size_ += bytes_;
+  }
+  return true;
+#  else
+  return await_ready();
+#  endif
+}
+#endif
+
+}  // namespace ice::net::tcp
---
+++ src/ice/net/tcp/socket.hpp
@@ -0,0 +1,205 @@
+#pragma once
+#include <ice/config.hpp>
+#include <ice/net/buffer.hpp>
+#include <ice/net/socket.hpp>
+#include <ice/service.hpp>
+#include <system_error>
+#include <utility>
+#include <cstddef>
+
+namespace ice::net::tcp {
+
+class accept;
+class connect;
+class recv;
+class send;
+class send_some;
+
+class socket : public net::socket {
+public:
+  explicit socket(ice::service& service) noexcept : net::socket(service) {}
+
+  socket(ice::service& service, int family);
+  socket(ice::service& service, int family, int protocol);
+
+  std::error_code create(int family) noexcept;
+  std::error_code create(int family, int protocol) noexcept;
+
+  std::error_code listen(std::size_t backlog = 0);
+
+  tcp::accept accept();
+  tcp::connect connect(const net::endpoint& endpoint);
+  tcp::recv recv(char* data, std::size_t size);
+  tcp::send send(const char* data, std::size_t size);
+  tcp::send_some send_some(const char* data, std::size_t size);
+};
+
+#if 0
+class accept final : public ice::event {
+public:
+#  if ICE_OS_WIN32
+  accept(tcp::socket& socket) noexcept :
+    service_(socket.service().handle()), socket_(socket.handle()),
+    client_(socket.service(), socket.family(), socket.protocol())
+  {}
+#  else
+  accept(tcp::socket& socket) noexcept :
+    service_(socket.service().handle()), socket_(socket.handle()), client_(socket.service())
+  {}
+#  endif
+
+  bool await_ready() noexcept;
+  bool suspend() noexcept override;
+  bool resume() noexcept override;
+
+  tcp::socket await_resume()
+  {
+    if (ec_) {
+      throw std::system_error(ec_, "accept tcp socket");
+    }
+    return std::move(client_);
+  }
+
+private:
+  ice::service::handle_view service_;
+  net::socket::handle_view socket_;
+  tcp::socket client_;
+#  if ICE_OS_WIN32
+  constexpr static unsigned long buffer_size = sockaddr_storage_size + 16;
+  char buffer_[buffer_size * 2];
+  unsigned long bytes_ = 0;
+#  endif
+};
+
+class connect final : public ice::event {
+public:
+  connect(tcp::socket& socket, const net::endpoint& endpoint) noexcept;
+
+  bool await_ready() noexcept;
+  bool suspend() noexcept override;
+  bool resume() noexcept override;
+
+  void await_resume()
+  {
+    if (ec_) {
+      throw std::system_error(ec_, "connect");
+    }
+  }
+
+private:
+  ice::service::handle_view service_;
+  net::socket::handle_view socket_;
+  const net::endpoint endpoint_;
+};
+
+class recv final : public ice::event {
+public:
+  recv(tcp::socket& socket, char* data, std::size_t size) noexcept :
+    service_(socket.service().handle()), socket_(socket.handle()), buffer_(data, size)
+  {}
+
+  bool await_ready() noexcept;
+  bool suspend() noexcept override;
+  bool resume() noexcept override;
+
+  std::size_t await_resume()
+  {
+    if (ec_) {
+      throw std::system_error(ec_, "tcp recv");
+    }
+    return static_cast<std::size_t>(buffer_.size);
+  }
+
+private:
+  ice::service::handle_view service_;
+  net::socket::handle_view socket_;
+  net::buffer buffer_;
+#  if ICE_OS_WIN32
+  unsigned long bytes_ = 0;
+  unsigned long flags_ = 0;
+#  endif
+};
+
+class send final : public ice::event {
+public:
+  send(tcp::socket& socket, const char* data, std::size_t size) noexcept :
+    service_(socket.service().handle()), socket_(socket.handle()), buffer_(data, size)
+  {}
+
+  bool await_ready() noexcept;
+  bool suspend() noexcept override;
+  bool resume() noexcept override;
+
+  std::size_t await_resume()
+  {
+    if (ec_) {
+      throw std::system_error(ec_, "tcp send");
+    }
+    return size_;
+  }
+
+private:
+  ice::service::handle_view service_;
+  net::socket::handle_view socket_;
+  net::const_buffer buffer_;
+  std::size_t size_ = 0;
+#  if ICE_OS_WIN32
+  unsigned long bytes_ = 0;
+#  endif
+};
+
+class send_some final : public ice::event {
+public:
+  send_some(tcp::socket& socket, const char* data, std::size_t size) noexcept :
+    service_(socket.service().handle()), socket_(socket.handle()), buffer_(data, size)
+  {}
+
+  bool await_ready() noexcept;
+  bool suspend() noexcept override;
+  bool resume() noexcept override;
+
+  std::size_t await_resume()
+  {
+    if (ec_) {
+      throw std::system_error(ec_, "tcp send some");
+    }
+    return size_;
+  }
+
+private:
+  ice::service::handle_view service_;
+  net::socket::handle_view socket_;
+  net::const_buffer buffer_;
+  std::size_t size_ = 0;
+#  if ICE_OS_WIN32
+  unsigned long bytes_ = 0;
+#  endif
+};
+
+inline tcp::accept socket::accept()
+{
+  return { *this };
+}
+
+inline tcp::connect socket::connect(const net::endpoint& endpoint)
+{
+  return { *this, endpoint };
+}
+
+inline tcp::recv socket::recv(char* data, std::size_t size)
+{
+  return { *this, data, size };
+}
+
+inline tcp::send socket::send(const char* data, std::size_t size)
+{
+  return { *this, data, size };
+}
+
+inline tcp::send_some socket::send_some(const char* data, std::size_t size)
+{
+  return { *this, data, size };
+}
+#endif
+
+}  // namespace ice::net::tcp
